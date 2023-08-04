bfc65263e2f62ea40f3852b62a8bc5ba6e50ff63
roy <roy93group@gmail.com>
2023-08-04 22:43:10 +0700

    up

--- app/src/main/java/com/roy93group/photoediting/PhotoApp.kt
+++ app/src/main/java/com/roy93group/photoediting/PhotoApp.kt
@@ -2,6 +2,21 @@ package com.roy93group.photoediting
 
 import android.app.Application
 
+//TODO applovin
+//TODO firebase
+
+//TODO ic_launcher
+//TODO manifest ad id
+//TODO proguard
+//TODO leak canary
+//TODO share app
+//TODO more app
+//TODO rate app
+//TODO policy
+//TODO keystore
+
+//done
+
 class PhotoApp : Application() {
     override fun onCreate() {
         super.onCreate()
